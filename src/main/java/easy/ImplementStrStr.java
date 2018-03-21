da14ec079351642a8de770d366e15d0218157b1f
liuchaoxin <a496418462@126.com>
2018-03-21 16:46:49 +0800

    28.Implement strStr()

---
+++ src/main/java/easy/ImplementStrStr.java
@@ -0,0 +1,33 @@
+package easy;
+
+public class ImplementStrStr {
+
+//    Implement strStr().
+//    Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+//    Example 1:
+//    Input: haystack = "hello", needle = "ll"
+//    Output: 2
+
+//    Example 2:
+//    Input: haystack = "aaaaa", needle = "bba"
+//    Output: -1
+
+    public static void main(String[] args) {
+        String haystack = "hello";
+        String needle = "o";
+        System.out.println(strStr(haystack,needle));
+    }
+
+    public static int strStr(String haystack, String needle) {
+        if(haystack == null || needle == null || haystack.length()<needle.length()) return -1;
+        if(needle.length() == 0) return 0;
+        int position = 0;
+        for(;position <= haystack.length() - needle.length();position++){
+            if(haystack.charAt(position) == needle.charAt(0)){
+               if(haystack.substring(position,position+needle.length()).equals(needle)) return position;
+            }
+        }
+        return -1;
+    }
+}
